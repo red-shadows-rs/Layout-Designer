37ea7812257688265294e64fb412708fa0a17508
NightcoreAT <oussamakatalonya1@gmail.com>
2025-07-22 21:10:09 +0100

    ✨| add main

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LayoutDesigner from './LayoutDesigner.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LayoutDesigner />
+  </React.StrictMode>
+)
